3aa5f22df08ad4e555c8d8807b9b3f1fa596de7a
Ford Fowler <fordjfowler@gmail.com>
2017-01-16 17:08:36 -0800

    receiver

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -22,7 +22,7 @@ class ImagerProfile(models.Model):
         ('URBAN', 'Urban'),
         ('PORTRAIT', 'Portrait'),
     )
-    camera_type = models.CharField(max_length=128, blank=True,)
+    camera_type = models.CharField(max_length=128, blank=True)
     address = models.CharField(max_length=255, blank=True, null=True)
     bio = models.TextField(blank=True,)
     website = models.CharField(max_length=255, blank=True,)
@@ -30,3 +30,12 @@ class ImagerProfile(models.Model):
     travel_radius = models.DecimalField(max_digits=5, decimal_places=2)
     phone = PhoneNumberField()
     type_of_photography = models.CharField(choices=PHOTOGRAPHY_CHOICES)
+
+    def active(self):
+        return ImagerProfile.objects.filter(ImagerProfile.is_active)
+
+
+@receiver(post_save, sender=User)
+def make_profile_from_user(sender, instance, **kwargs):
+    new_profile = ImagerProfile(user=instance)
+    new_profile.save()
--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'imager_profile'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'imagersite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'midfies',
+        'NAME': os.environ['USERNAME'],
         'USER': os.environ['DATABASE_PW'],
         'HOST': '127.0.0.1',
         'PORT': '5432',
