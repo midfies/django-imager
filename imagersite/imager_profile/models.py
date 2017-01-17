dc559a98f641f49208a128d06838f62d20aac28d
Marc Fieser <midfies@gmail.com>
2017-01-16 16:46:39 -0800

    added new app files, started on models

---
+++ imagersite/imager_profile/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+# Create your models here.
+
+class ImagerProfile(models.Model):
+    """The library Patro and all of its attributes."""
+
+    user = models.OneToOneField(
+        User,
+        related_name='profile',
+        on_delete=models.CASCADE,
+    )
+    NATURE = 'Nature'
+    URBAN = 'Urban'
+    PORTRAIT = 'Portrait'
+
+    PHOTOGRAPHY_CHOICES = (
+        (NATURE, 'Nature'),
+        (URBAN, 'Urban'),
+        (PORTRAIT, 'Portrait'),
+    )
+    camera_type = models.CharField(max_length=128)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    bio = models.TextField()
+    website = models.CharField(max_length=255)
+    hireable = models.BooleanField(default=True)
+    travel_radius = models.DecimalField(max_digits=5, decimal_places=2)
+    phone = PhoneNumberField()
+    type_of_photography = models.CharField(choices=PHOTOGRAPHY_CHOICES)
--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'imagersite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'midfies',
+        'USER': os.environ['DATABASE_PW'],
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'TEST': {
+            'NAME': 'test'
+        }
     }
 }
 
@@ -105,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
