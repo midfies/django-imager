4f1070d9e142ec366aac142f15225b727c553269
Marc Fieser <midfies@gmail.com>
2017-01-24 18:13:37 -0800

    profileView is now detail view

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from imager_profile.views import ProfileView
 
 urlpatterns = [
-    url(r'(?P<username>\w+)/$', ProfileView.as_view(), name='profile'),
+    url(r'(?P<slug>\w+)/$', ProfileView.as_view(), name='profile'),
     url(r'^$', ProfileView.as_view(), {'username': 'USER'}, name='profile'),
 ]
--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,19 +1,20 @@
 """Views for profile page."""
 from django.contrib.auth.models import User
 from imager_profile.models import ImagerProfile
-from django.views.generic import TemplateView
+from django.views.generic import DetailView
 
 
-class ProfileView(TemplateView):
+class ProfileView(DetailView):
     """"ProfileView."""
 
     template_name = 'imager_profile/profile.html'
     model = ImagerProfile
+    slug_field = 'user__username'
 
     def get_context_data(self, **kwargs):
         """Get profile information and return it."""
         context = super(ProfileView, self).get_context_data(**kwargs)
-        profile = ImagerProfile.active.get(user__username=self.kwargs['username'])
+        profile = ImagerProfile.active.get(user__username=self.kwargs['slug'])
         context['profile'] = profile
-        context['userdata'] = User.objects.filter(username=self.kwargs['username']).first()
+        context['userdata'] = User.objects.filter(username=self.kwargs['slug']).first()
         return context
