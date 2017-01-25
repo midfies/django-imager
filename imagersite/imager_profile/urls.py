8e09decd6620ac4d487918825faaa24d5c4e2a1a
Marc Fieser <midfies@gmail.com>
2017-01-24 18:36:46 -0800

    fixed image on home page

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -1,8 +1,8 @@
 """URLS for profile routes."""
 from django.conf.urls import url
-from imager_profile.views import ProfileView
+from imager_profile.views import ProfileView, ProfileUserView
 
 urlpatterns = [
     url(r'(?P<slug>\w+)/$', ProfileView.as_view(), name='profile'),
-    url(r'^$', ProfileView.as_view(), {'username': 'USER'}, name='profile'),
+    url(r'^$', ProfileUserView.as_view(), name='profile_user'),
 ]
--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,7 +1,7 @@
 """Views for profile page."""
 from django.contrib.auth.models import User
 from imager_profile.models import ImagerProfile
-from django.views.generic import DetailView
+from django.views.generic import DetailView, TemplateView
 
 
 class ProfileView(DetailView):
@@ -13,8 +13,24 @@ class ProfileView(DetailView):
 
     def get_context_data(self, **kwargs):
         """Get profile information and return it."""
+
         context = super(ProfileView, self).get_context_data(**kwargs)
         profile = ImagerProfile.active.get(user__username=self.kwargs['slug'])
         context['profile'] = profile
         context['userdata'] = User.objects.filter(username=self.kwargs['slug']).first()
+        return context
+
+
+class ProfileUserView(TemplateView):
+    """"ProfileView."""
+
+    template_name = 'imager_profile/profile.html'
+    model = ImagerProfile
+
+    def get_context_data(self, **kwargs):
+        """Get profile information and return it."""
+        context = super(ProfileUserView, self).get_context_data(**kwargs)
+        profile = ImagerProfile.active.get(user__username=self.request.user)
+        context['profile'] = profile
+        context['userdata'] = User.objects.filter(username=self.request.user).first()
         return context
